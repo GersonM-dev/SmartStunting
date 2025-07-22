99a3a690b90dedf36ee0b8944df6d112a46eb097
Gerson Manuel Sugianto <dev.gersonmanuel@gmail.com>
2025-07-22 10:30:15 +0700

    feat: Add delete action to AnakResource and AntropometryRecordResource tables

--- app/Filament/Resources/AntropometryRecordResource.php
+++ app/Filament/Resources/AntropometryRecordResource.php
@@ -115,6 +115,7 @@ class AntropometryRecordResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
